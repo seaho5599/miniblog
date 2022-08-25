7e9f5f5d4cafff8996b7eb6566f9b5e9d5e86890
seaho5599 <seaho5599@gmail.com>
2022-08-25 11:48:13 +0900

    html 초기화, css 초기화, favicon교체

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrap">
+    <BlogHeader/>
+    <BlogInput/>
+    <BlogList/>
+    <BlogFooter/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogHeader from '@/components/BlogHeader.vue';
+import BlogInput from '@/components/BlogInput.vue';
+import BlogList from '@/components/BlogList.vue';
+import BlogFooter from '@/components/BlogFooter.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      BlogHeader,
+      BlogInput,
+      BlogList,
+      BlogFooter,
+    },
+    setup() {
+      return {}
+    }
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@charset 'utf-8';
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+*{
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+}
+ul, li{
+  list-style: none;
+}
+img{
+  vertical-align: middle;
+  border: 0
+}
+a{
+  text-decoration: none;
+  color: #333;
+}
+html{
+  font-size: 16px;
+  overflow-x: hidden;
+}
+body{
+  font-family: 'Montserrat', 'Noto Sans KR', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.2;
+  letter-spacing: -0.4px;
+  color: #000;
 }
 </style>
