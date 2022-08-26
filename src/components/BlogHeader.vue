960395d1d60cf05b96008da7b2308e7a671d40bf
seaho5599 <seaho5599@gmail.com>
2022-08-26 13:56:54 +0900

    내용수정 버튼

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -1,19 +1,46 @@
 <template>
   <header class="header">
     <h1>Header</h1>
+    <p>
+      <span>{{timestamp}}</span>
+      </p>
   </header>
 </template>
 
 <script>
 export default {
+  data(){
+      return{
+      timestamp:""  
+      }
+    },
+    created(){
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    const weekList = new Array("Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.");
+    const week = weekList[now.getDay()];
+    this.timestamp = `${month}/${date} ${week}`;
+    },
 
+  setup(){
+    
+    return{
+
+    }
+  }
 }
 </script>
 
 <style scoped>
   h1{
     font-weight: 700;
-    text-align: center;
+    text-align: center; 
+  }
+  p{
+    display: inline-block;
+    width: 100%;
+    text-align: right;
     
   }
 </style>
--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,51 +1,70 @@
 <template>
   <div class="list-wrap">
     <ul>
-      <!-- v-for in :key -->
-      <li v-for="(item, index) in memoItemArr" :key="index" class="shadow">
-        {{item}}
-        <span class="remove-bt">
-          <i class="far fa-trash-alt" @click="removeMemo(item, index)"></i>
-        </span>
-      </li>
+        <li v-for="(item, index) in memoItemArr" v-bind:key="index" class="shadow"> 
+          <i class="fas fa-edit check-bt" @click="updateMemo(item, index)" :class="{memoComplete:item.complete}"></i>
+          
+          <span :class="{memoCompleteTxt:item.complete}">{{item.memotitle}}</span>
+
+          <span class="remove-bt" @click="removeMemo(item.id, index)">
+            <i class="far fa-trash-alt"></i>
+          </span>
+        </li>  
     </ul>
+
   </div>
 </template>
 
 <script>
-  import {
-    ref,
-    reactive
-  } from 'vue'
-  export default {
-    setup() {
-      // localstorage 의 목록을 가지고 오기
-      // console.log(localStorage)
-      // 전체 개수
-      const total = ref(0);
-      total.value = localStorage.length
-      console.log(total.value)
-      // 키네임을 저장하는 배열
-      const memoItemArr = reactive([])
-      if (total.value > 0) {
-        for (let i = 0; i < total.value; i++) {
-          // 배열에 요소를 밀어넣는다.
-          memoItemArr.push(localStorage.key(i))
-        }
-        
+import {ref, reactive} from 'vue';
+export default {
+  setup() {
+    // localstorage 의 목록을 가지고 오기
+    // console.log(localStorage);
+    // 전체 개수
+    const total = ref(0);
+    total.value = localStorage.length;
+    console.log(total.value);
+
+    // 키네임을 저장하는 배열
+    const memoItemArr = reactive([]);
+    if( total.value > 0) {
+      for(let i = 0; i < total.value; i++) {
+        // 배열에 요소를 밀어넣는다.
+        // 키값도 필요하지만, 실제 내용(값)이 필요하다.
+        let obj = localStorage.getItem( localStorage.key(i) );
+        console.log(obj);
+        memoItemArr.push(JSON.parse(obj));
       }
-      const removeMemo = (item, index) => {
-        // localStrage 에서 key를 통해서 지운다.
-        localStorage.removeItem(item);
-        // 배열(memoItemArr) 에서도 지운다.
-        memoItemArr.splice(index, 1)
-      }
-      return {
-        memoItemArr,
-        removeMemo
-      }
+      // 키값을 이용해서 정렬하기(오름차순)
+      memoItemArr.sort();
     }
+
+    const removeMemo = (item, index) => {
+      // localStrage 에서 key를 통해서 지운다.
+      localStorage.removeItem(item);
+      // 배열(memoItemArr) 에서도 지운다.
+      memoItemArr.splice(index, 1);
+    }
+    const updateMemo = (item) => {
+      // localStroage 에서는 update메소드를 지원하지 않는다.
+      // 찾아서 지우고, 
+      localStorage.removeItem(item.id);
+      // 변경한다.
+      item.complete = !item.complete;
+      //다시 set 한다.
+      localStorage.setItem(item.id, JSON.stringify(item));
+
+    }
+    
+    return {      
+      memoItemArr,
+      removeMemo,
+      updateMemo
+    }
+
   }
+}
 </script>
 
 <style scoped>
@@ -54,7 +73,7 @@
     min-height: 50px;
     line-height: 50px;
     margin: 10px 0;
-    background: #fff;
+    background-color: #fff;
     border-radius: 5px;
     padding: 0 20px;
   }
@@ -63,5 +82,19 @@
     cursor: pointer;
     margin-left: auto;
     color: hotpink;
+  }
+  .check-bt{
+    color: #62acde;
+    line-height: 50px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  .memoComplete{
+    color: #63adad;
+
+  }
+  .memoCompleteTxt{
+    color: #63adad;
+    text-decoration: line-through;
   }
 </style>
